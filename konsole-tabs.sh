89adcb726cbb51716adcd1098f613f59cd46e42a
Sergii Bieliaievskyi <sergii.bieliaievskyi@4finance.com>
2017-08-03 21:00:56 +0200

    Optimization. tested with Konsole 14.04 and 16.04

--- konsole-tabs.sh
+++ konsole-tabs.sh
@@ -1,35 +1,20 @@
 #! /bin/bash
 
-#
-# Written for KDE /usr/bin/konsole command.
-# For opening new tabs in existing konsole.
-#
-
 NewWindow=0
 kses=$(qdbus | grep konsole | head -n 1)
-if [[ "$kses" == "" ]]; then
+if [[ $kses == "" ]]; then
   NewWindow=1
   /usr/bin/konsole
   kses=$(qdbus | grep konsole | head -n 1)
 fi
-echo $kses >/tmp/kses
-echo >>/tmp/kses
-qdbus $kses >>/tmp/kses || exit
-echo >>/tmp/kses
-qdbus $kses /Konsole >>/tmp/kses
-echo >>/tmp/kses
 for i in $*
 do
-    echo >>/tmp/kses
     if [[ $NewWindow == 0 ]]; then
-      session=$(qdbus $kses /Konsole newSession)
+      session=$(qdbus $kses /Windows/1 newSession)
     else
-      session=$(qdbus $kses /Konsole currentSession)
+      session=$(qdbus $kses /Windows/1 currentSession)
     fi
-    echo $session
-    echo $session >>/tmp/kses
-    qdbus $kses /Sessions/${session} >>/tmp/kses
-    qdbus $kses /Sessions/${session} sendText "ssh user@${i}
+    qdbus $kses /Sessions/${session} sendText "jump.sh ${i}
 "
     qdbus $kses /Sessions/${session} setMonitorSilence true
 done
