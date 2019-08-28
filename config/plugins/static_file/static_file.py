ec1cc663a4c2a4d26df454b59a072802d49df442
Sergii Bieliaievskyi <magelan09@gmail.com>
2019-08-28 09:34:13 +0200

    load static inventory properly

--- config/plugins/static_file/static_file.py
+++ config/plugins/static_file/static_file.py
@@ -1,3 +1,8 @@
 class Inventory:
   def __init__(self):
     self.nodes_list = []
+  def match_nodes(self, filter_string):
+    tmp_nodes = self.nodes_list
+    for word in filter_string.split(' '):
+        tmp_nodes = list(filter(lambda hs: word in hs['words'], tmp_nodes))
+    return tmp_nodes
